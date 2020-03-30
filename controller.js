27dd74d886af32abc8799cc052857a5917a9094f
Rivaldo Avila <rivaldoavila@outlook.com>
2020-03-30 19:15:51 +0700

    berhasil menampilkan data mahasiswa

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function (req, res) {
-    response.ok("Aplikasi REST API ku Berjalan", res)
+    response.ok("Aplikasi REST API ku Berjalan",res)
+};
+
+//Menambahkan data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
 };
